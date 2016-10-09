d1ed7738e4f8b4dc5e2241c9490ad5f49e61ef94
Vinoth Kumar Asaithambi <me@geekyvin.com>
2016-10-09 21:46:55 +0100

    Added Excercise 1 Main class

---
+++ src/main/java/org/monksandbots/playground/geneticalgo/AllOnesGA.java
@@ -0,0 +1,11 @@
+package org.monksandbots.playground.geneticalgo;
+
+public class AllOnesGA {
+
+    public static void main(String args[]) {
+        //Create GA object
+        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01, 0.95, 0);
+        //ToDo: Add a lot more here
+    }
+
+}
